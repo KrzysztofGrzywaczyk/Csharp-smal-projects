c5f0150f686a4dc8088b970a586e7b0aadfed718
Krzysztof Grzywaczyk <krzysztof.grzywaczyk@gmail.com>
2023-03-29 20:21:17 +0200

    Add FizBuzz with Tests

---
+++ FizzBuzz algorythm with Tests/FizzBazzAlgorythmTests/FizzBuzzTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using FizzBuzz_algorithm;
+
+namespace FizzBuzzAlgorythmTests
+{
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void FizzBuzz_WhenArgumentDivisibleBy3and5_ShouldReturnFizzBuzz()
+        {
+            var result = FizzBuzz.FizzBuzzMethod(15);
+            Assert.AreEqual(result,"FizzBuzz");
+        }
+
+        [Test]
+        public void FizzBuzz_WhenArgumentDivisibleBy3_ShouldReturnFizz()
+        {
+            var result = FizzBuzz.FizzBuzzMethod(3);
+            Assert.AreEqual(result, "Fizz");
+
+        }
+
+        [Test]
+        public void FizzBuzz_WhenArgumentDivisibleBy5_ShouldReturnBuzz()
+        {
+            var result = FizzBuzz.FizzBuzzMethod(5);
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [Test]
+        public void FizzBuzz_WhenArgumentNotDivisibleBy3or5_ShouldReturnArgument()
+        {
+            var result = FizzBuzz.FizzBuzzMethod(2);
+            Assert.AreEqual(result, "2");
+        }
+    }
+}
---
+++ FizzBuzz algorythm with Tests/FizzBuzz algorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using FizzBuzz_algorithm;
+
+/* FizzBuzz is a popular algorithm whose implementation ability tests the skills of novice programmers.
+    FizzBuzz method is designed to print:
+    - FizzBuzz when number is divisible by 3 and 5
+    - Fizz when number is divisible by 5
+    - Buzz when number is divisible by 3
+    - Given number if the number is not divisible by 3 or 5     */
+
+
+Console.WriteLine(FizzBuzz.FizzBuzzMethod(30));
+Console.WriteLine(FizzBuzz.FizzBuzzMethod(5));
+Console.WriteLine(FizzBuzz.FizzBuzzMethod(3));
+Console.WriteLine(FizzBuzz.FizzBuzzMethod(1));
+
